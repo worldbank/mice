7a6c44852523f5bc8f7254135e38a833e493f9a3
Sardar Azari <sazari@worldbankgroup.org>
2018-06-06 16:32:44 -0400

    implemented parallel wrapper for mice

---
+++ R/mice_par.R
@@ -0,0 +1,33 @@
+# 'Parallel wrapper for mice'
+#
+#'@export
+mice_par <- function(data, m = 5, 
+                 method = NULL,
+                 predictorMatrix,
+                 where = NULL,
+                 blocks,
+                 visitSequence = NULL,
+                 formulas,
+                 blots = NULL,
+                 post = NULL,
+                 defaultMethod = c("pmm", "logreg", "polyreg", "polr"),
+                 maxit = 5, printFlag = TRUE, seed = NA,
+                 data.init = NULL,
+                 ...) {
+
+  retVaL <- mice (data, m = 5, 
+                       method = NULL,
+                       predictorMatrix,
+                       where = NULL,
+                       blocks,
+                       visitSequence = NULL,
+                       formulas,
+                       blots = NULL,
+                       post = NULL,
+                       defaultMethod = c("pmm", "logreg", "polyreg", "polr"),
+                       maxit = 5, printFlag = TRUE, seed = NA,
+                       data.init = NULL,
+                       ...)
+    
+  return (retVal)
+  }
